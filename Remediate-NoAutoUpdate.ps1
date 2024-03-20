42931f1dd0f2c84a1b60d1b4c01c3c6f97f6db45
Hal-Scriptville <161669964+Hal-Scriptville@users.noreply.github.com>
2024-03-20 13:06:05 -0400

    Add files via upload
    Windows Auto Update check and remediation script for Intune.

---
+++ Remediate-NoAutoUpdate.ps1
@@ -0,0 +1,18 @@
+# Define the registry path and value
+$registryPath = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"
+$valueName = "NoAutoUpdate"
+$expectedValue = "0"
+
+
+# Set the registry value
+try {
+    Set-ItemProperty -Path $registryPath -Name $valueName -Value $correctValue
+    Write-Host "Registry value corrected."
+} catch {
+    Write-Error "Error occurred while setting registry value."
+    exit 1
+}
+
+# Exit codes:
+# 0 - Remediation successful
+# 1 - Remediation failed
