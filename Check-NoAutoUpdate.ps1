d7d2e66eb559c4230f1f19d06d7ca1716c4de385
Hal-Scriptville <161669964+Hal-Scriptville@users.noreply.github.com>
2024-03-20 13:06:05 -0400

    Add files via upload
    Windows Auto Update check and remediation script for Intune.

---
+++ Check-NoAutoUpdate.ps1
@@ -0,0 +1,26 @@
+# Define the registry path and value
+$registryPath = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"
+$valueName = "NoAutoUpdate"
+$expectedValue = "1"
+
+
+# Check if the registry path exists
+if (Test-Path $registryPath) {
+    # Get the current value
+    $currentValue = Get-ItemPropertyValue -Path $registryPath -Name $valueName -ErrorAction SilentlyContinue
+
+    # Check if the value is as expected
+    if ($currentValue -eq $expectedValue) {
+        # Return non-compliant status if value is not as expected
+        Write-Host "NonCompliant"
+		exit 1
+    } else {
+        # Return compliant status if value is as expected
+        Write-Host "Compliant"
+		exit 0
+    }
+} else {
+    # Return non-compliant status if registry path doesn't exist
+    Write-Host "Compliant"
+	exit 0
+}
