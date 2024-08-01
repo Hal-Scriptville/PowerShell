5e44df97e31355d4eee373ebf2a0d743729176ff
Hal-Scriptville <161669964+Hal-Scriptville@users.noreply.github.com>
2024-08-01 12:55:47 -0400

    Update Detect-IntuneCache.ps1
    Message output added

--- Detect-IntuneCache.ps1
+++ Detect-IntuneCache.ps1
@@ -3,7 +3,9 @@
 $msiFiles = Get-ChildItem -Path 'C:\Windows\System32\config\systemprofile\AppData\Local\mdm' -Filter *.msi -File -ErrorAction SilentlyContinue
 
 if ($msiFiles.Count -gt 0) {
+    write-host "Remediation needed" -foregroundcolor red
     exit 1  # Remediation needed
 } else {
+    write-host "No remediation needed" -foregroundcolor green
     exit 0  # No remediation needed
 }
